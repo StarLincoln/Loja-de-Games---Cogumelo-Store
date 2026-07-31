9b23b53da66a395dee8b05a5322524b80353ef9d
Caio Lincoln <caiolincolnazevedo@gmail.com>
2026-07-31 02:53:25 -0300

    Finalizei o entities

--- src/entities/produto.ts
+++ src/entities/produto.ts
@@ -1,9 +1,67 @@
-export interface IEntidade { id: number}
+export interface IEntidade {
+  id: number;
+}
 
-export interface Produto {
-    id: number;
-    nome: string;
-    lancamento: string;
-    plataforma: string;
-    avaliacao: number | null;
+export class Produto implements IEntidade {
+  constructor(
+    public _id: number,
+    private _nome: string,
+    private _preco: number,
+    private _lancamento: number,
+    private _plataforma: string,
+    private _avaliacao: number | null,
+  ) {}
+
+  get id(): number {
+    return this._id
+  }
+  get nome(): string {
+    return this._nome;
+  }
+  get preco(): number {
+    return this._preco;
+  }
+  get lancamento(): number {
+    return this._lancamento;
+  }
+  get plataforma(): string {
+    return this._plataforma;
+  }
+  get avaliacao(): number | null {
+    return this._avaliacao;
+  }
+
+  set nome(dado: string) {
+    if (!dado || dado.trim() === "") throw new Error("Erro! Preencha o nome");
+    this._nome = dado.trim();
+  }
+  set preco(dado: number) {
+    if (dado <= 0) throw new Error("Erro! O preço não pode ser negativo");
+    this._preco = dado;
+  }
+  set lancamento(dado: number) {
+    if (dado < 1950 || dado > Number(new Date().getFullYear()))
+      throw new Error("Erro! Ano inválido");
+    this._lancamento = dado;
+  }
+  set plataforma(dado: string) {
+    if (!dado || dado.trim() === "") throw new Error("Erro! Preencha o plataforma");
+    this._plataforma = dado.trim();
+  }
+  set avaliacao(dado: number | null) {
+    this._avaliacao = dado;
+  }
+  static fromJson(json: any): Produto{
+    return new Produto(json.id, json.nome, json.preco, json.lancamento, json.plataforma, json.avaliacao ?? null)
+  }
+  toJson(): object {
+    return {
+      id: this._id,
+      nome: this._nome,
+      preco: this._preco,
+      lancamento: this._lancamento,
+      plataforma: this._plataforma,
+      avaliacao: this._avaliacao
+    }
+  }
 }
--- src/models/produtoModel.ts
+++
@@ -1,2 +0,0 @@
-import { Produto } from "../entities/produto";
-
