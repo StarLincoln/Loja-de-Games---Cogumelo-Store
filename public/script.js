de7a93640b6e36a5e5779c75d9dafcb87eda6bbd
João Maria <joaomariafilho531@gmail.com>
2026-08-03 17:43:16 -0300

    melhoria completa nos botões

--- public/script.js
+++ public/script.js
@@ -1,19 +1,55 @@
+if (
+    localStorage.getItem("logado") === "true" &&
+    (window.location.pathname.includes("login.html") ||
+     window.location.pathname.includes("index.html"))
+) {
+    window.location.href = "loja.html";
+}
+if (
+    window.location.pathname.includes("loja.html") &&
+    localStorage.getItem("logado") !== "true"
+) {
+    window.location.href = "login.html";
+}
 
-const btnIrLogin = document.getElementById('btnIrLogin');
+const btnIrLogin = document.getElementById("btnIrLogin");
+
 if (btnIrLogin) {
-    btnIrLogin.addEventListener('click', () => {
-        window.location.href = 'login.html';
+    btnIrLogin.addEventListener("click", () => {
+        window.location.href = "login.html";
     });
 }
 
-const btnAutenticar = document.getElementById('btnAutenticar');
+const btnAutenticar = document.getElementById("btnAutenticar");
 
 if (btnAutenticar) {
-    btnAutenticar.addEventListener('click', () => {
 
-        alert("Login feito");
+    const email = document.querySelector('input[type="email"]');
+    const senha = document.querySelector('input[type="password"]');
+
+    btnAutenticar.addEventListener("click", () => {
+
+        if (email.value.trim() === "" || senha.value.trim() === "") {
+            alert("Preencha o e-mail e a senha.");
+            return;
+        }
+
+        localStorage.setItem("logado", "true");
+        localStorage.setItem("email", email.value);
+
+        alert("Login feito com sucesso!");
 
         window.location.href = "loja.html";
+    });
+}
 
+const btnSair = document.getElementById("btnSair");
+
+if (btnSair) {
+    btnSair.addEventListener("click", () => {
+        localStorage.removeItem("logado");
+        localStorage.removeItem("email");
+    
+        window.location.href = "index.html";
     });
 }
